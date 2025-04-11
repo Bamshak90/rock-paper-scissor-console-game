de79f2ee0ac1e831fb60b51fe6ee203abb8b0462
Bamshak90 <lilmarkhat@gmail.com>
2025-04-11 13:39:17 +0100

    Made changes to fix a bug that allows moves to overlap. And created two functions

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-console.log('Hello World')
+
 
 // Creating a function that allows the computer to randomly  generate a move
+
+let humanScore = 0;
+
+let computerScore = 0;
+
 
 function getComputerMove() {
   const randomNumber = Math.random();
 
-  if (randomNumber == 0 || randomNumber <= 1/3) {
-      console.log("Rock")
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+  
+    console.log("Rock");
+    
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
 
-  } else if (randomNumber == 1/3 || randomNumber <= 2/3) {
       console.log("Paper");
 
-  } else if ( randomNumber == 2/3 || randomNumber <= 1) 
-      console.log("Scissors")
+  } else if ( randomNumber >= 2/3 && randomNumber < 1) {
+
+    console.log("Scissors");
+  }
+
+    
   
 
   // console.log(randomNumber)
@@ -21,3 +32,13 @@ function getComputerMove() {
 }
 
 getComputerMove()
+
+function getHumanChoice() {
+  const userMove = prompt("Enter your move: Rock, Paper, Scissors");
+}
+// getHumanChoice()
+
+function playRound(humanChoice, computerChoice) {
+
+
+}

dd46b19ddc58ca70275dce0018ea8271a33fbad3
Bamshak90 <lilmarkhat@gmail.com>
2025-04-11 12:13:45 +0100

    Successful created a function that randomly generates the computer move using Math.random() function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('Hello World')
+
+// Creating a function that allows the computer to randomly  generate a move
+
+function getComputerMove() {
+  const randomNumber = Math.random();
+
+  if (randomNumber == 0 || randomNumber <= 1/3) {
+      console.log("Rock")
+
+  } else if (randomNumber == 1/3 || randomNumber <= 2/3) {
+      console.log("Paper");
+
+  } else if ( randomNumber == 2/3 || randomNumber <= 1) 
+      console.log("Scissors")
+  
+
+  // console.log(randomNumber)
+
+
+}
+
+getComputerMove()
